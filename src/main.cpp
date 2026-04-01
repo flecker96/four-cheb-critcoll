54b69c585fae8bfb101191ce9743a1fcc2abda37
flecker96 <eckerflorian32@gmail.com>
2026-04-01 09:06:29 +0200

    added sample_data

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,26 @@
 
 int main(){
 
-    std::string inputPath = "../to_hdf5/data_128_14D.h5";
-    std::string outputPath = "../to_hdf5/data_out.h5";
+    std::string inputPath = "../sample_data/data_128_8D.h5";
+    std::string outputPath = "../sample_data/data_out.h5";
 
     SimulationConfig config = SimulationConfig::loadFromHDF5(inputPath);
 
-    //Double Nt
+    /*//Double Nt
     SimulationConfig confignew(2*config.Nt, config.Nx);
     {
         Sampler samp(config.Nt, config.Nx);
         samp.double_Nt(config, confignew);
-    }
+    }*/
 
     //config.Dim = 14.0;
-    confignew.EpsNewton = 1E-5;
-    confignew.Converged = false;
-    config.PrecisionNewton = 1E-2;
-    confignew.print_config();
+    config.EpsNewton = 1E-8;
+    config.Converged = false;
+    config.PrecisionNewton = 1E-12;
+    config.print_config();
 
-    SimulationConfig result(confignew.Nt, confignew.Nx);
-    NewtonSolver solver(confignew, result);
+    SimulationConfig result(config.Nt, config.Nx);
+    NewtonSolver solver(config, result);
     solver.run();
     
     result.writeToHdf5(outputPath);
