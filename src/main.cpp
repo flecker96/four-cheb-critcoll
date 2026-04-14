a1b4066627c1582b7ed5f134cca075fa3ede9609
flecker96 <eckerflorian32@gmail.com>
2026-04-14 13:45:48 +0200

    added svd check

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,19 @@
 
 int main(){
 
-    std::string inputPath = "../sample_data/5.5D_data.h5";
-    std::string outputPath = "../sample_data/data_out.h5";
+    std::string inputPath = "../to_hdf5/singular_vectors/6D/160128_6D.h5";
+    std::string outputPath = "../to_hdf5/singular_vectors/6D/160256_6D.h5";
 
     SimulationConfig configIn = SimulationConfig::loadFromHDF5(inputPath);
 
     //Add(pad with zeros) or delete modes, Ntnew = fact * Nt, Nxnew = facx * Nx
-    real_t fact = 1.0, facx = 1.0;
+    real_t fact = 2.0, facx = 1.0;
     SimulationConfig config = changeModes(configIn, fact, facx);
     
-    //config.Dim = 20.0;
-    //config.EpsNewton = 1E-6;
-    //config.Converged = false;
-    //config.PrecisionNewton = 1E-13;
+    //config.Dim = 7.0;
+    config.EpsNewton = 1E-6;
+    config.Converged = false;
+    config.PrecisionNewton = 1E-13;
     config.print_config();
 
     SimulationConfig result(config.Nt, config.Nx);
